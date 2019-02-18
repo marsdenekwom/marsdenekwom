3c0b9c40b56683cd184dcb539b66abd8c9106b1e
Marsden Ekwom <nabuinekwom@gmail.com>
2019-02-18 09:49:12 +0300

    added javascript content to the html

--- js/tracker.js
+++
@@ -1,9 +0,0 @@
-if(sides[0] == sides[1] && sides[1] == sides[2]){
-        System.out.println("It's an equilateral triangle.");
-    }
- else if(sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2]){
-        System.out.println("It's an isosceles triangle.");
-    }
-else(sides[0] ==! sides[1] || sides[1] ==! sides[2] || sides[0] ||sides[2]{
-        System.out.println("Its a scalene triangle");
-}
