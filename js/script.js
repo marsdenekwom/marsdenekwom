fe48a092a8f1c3de104c056dc252e1ffb36ca260
Marsden Ekwom <nabuinekwom@gmail.com>
2019-02-18 10:44:22 +0300

    making the results show on the htm file

--- js/script.js
+++ js/script.js
@@ -2,25 +2,24 @@ function triangleSides() {
 
   var result = [];
 
-
-  var m = parseInt(document.getElementById('M').value);
-  result.push(m);
-  var n = parseInt(document.getElementById('N').value);
-  result.push(n);
-  var o = parseInt(document.getElementById('O').value);
-  result.push(o);
+  var a = parseInt(document.getElementById('A').value);
+  result.push(a);
+  var b = parseInt(document.getElementById('B').value);
+  result.push(b);
+  var c = parseInt(document.getElementById('C').value);
+  result.push(c);
   var display = document.querySelector("#results");
 
   //Check if input is a triangle
-  if((m+n>c) && (n+o>m)&& (m+o>n)) {
-    if(m == n && m ==o && n == o) {
+  if(a+b>c && b+c>a && a+c>b) {
+    if(a == b && a ==c && b == c) {
       display.innerHTML ="IT IS AN EQUILATERAL TRIANGLE"
     }
-    else if (m == n || m ==o || n == o) {
+    else if (a == b || a ==c || b == c) {
       display.innerHTML = "IT IS AN ISOSCELES TRIANGLE"
     }
     else {
       display.innerHTML = "IT IS A SCALENE TRIANGLE"
     }
   }
-};
+}
